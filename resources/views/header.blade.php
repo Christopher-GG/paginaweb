35245ff88b190bf6521e3a61648c68aabc9e503d
Christopher G.G <cgg_chan@outlook.com>
2021-05-15 22:44:39 -0500

    Iniciando repositorio

---
+++ resources/views/header.blade.php
@@ -0,0 +1,8 @@
+<div class="row">
+    <div class="col col-lg-12 titulo">
+        <img src="imagenes/Fox.png" width="100px">
+        FoxAnime
+    </div>
+</div>
+
+<div class="row espacio"></div>
